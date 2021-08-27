fca598c147196dfc3a70d707df4b5129c031d232
Joseph Amador <kirito.1986.714@gmail.com>
2021-08-27 10:02:27 -0700

    added user schema and added express validator and add check for user.js

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,33 @@
 const express = require("express");
 const router = express.Router();
+// middleWare Validator to check isEmail ect.
+const { body, validationResult, check } = require("express-validator");
+
+const User = require("../models/User");
 
 //@route  Post api/users
 //@desc   Register a user
 //@access Public
-router.post("/", (req, res) => {
-  res.send("Registers a user");
-});
+router.post(
+  "/",
+  [
+    // use Express Validator to check name
+    check("name", "Please add name").not().isEmpty(),
+    // check email
+    check("email", "Please include a valid email").isEmail(),
+    // check to make sure password is more then 6 characters long
+    check(
+      "password",
+      "Please enter a email with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("passed");
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+// connect DataBase
+connectDB();
+
+//init Middleware
+// makes it so we can accept body Data
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) =>
   res.json({ msg: `Welcome to my contactKeeper API` })
@@ -11,6 +18,6 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 
-const PORT = process.env.POT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port${PORT}`));
